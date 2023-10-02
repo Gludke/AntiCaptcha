1d19fa4ab1570e9981ba67f98666d303b432a4fa
Guilherme Ludke <guiludke@gmail.com>
2023-10-02 03:27:00 -0300

    a

--- src/AntiCaptcha.Api/Controllers/WeatherForecastController.cs
+++ src/AntiCaptcha.Api/Controllers/WeatherForecastController.cs
@@ -46,7 +46,7 @@ namespace AntiCaptcha.Api.Controllers
                     AcceptDownloads = true,
                     Locale = "pt-BR",
                     //UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
-                    UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4500.0 Safari/537.36 Edge/100.0.10586.0"
+                    UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.92 Safari/537.36 Edge/100.0.10586.0"
                 });
 
             await Page.GotoAsync("https://cvmweb.cvm.gov.br/SWB/default.asp?sg_sistema=scw");
@@ -54,33 +54,33 @@ namespace AntiCaptcha.Api.Controllers
             await Page.FrameLocator("frame[name=\"Main\"]").FrameLocator("frame[name=\"SubMain\"]").GetByRole(AriaRole.Link, new() { Name = "Acesso Gov BR" }).ClickAsync();
             await Task.Delay(1000);
 
-            //Interação extra, para acumular cache
-            await Page.GetByRole(AriaRole.Link, new() { Name = "Logomarca GovBR" }).ClickAsync();
-            await Page.GetByRole(AriaRole.Button, new() { Name = " Entrar com o gov.br" }).ClickAsync();
-
-            // Simula um movimento do mouse
-            await Page.Mouse.MoveAsync(100, 200);
-            await Task.Delay(1000);
-
             await Page.GetByPlaceholder("Digite seu CPF").ClickAsync();
             await Page.Keyboard.TypeAsync("157.010.968-07", new() { Delay = 100 }); // Adiciona um atraso entre os caracteres para simular a digitação
 
             await Page.GetByRole(AriaRole.Button, new() { Name = "Continuar" }).ClickAsync();
 
-            // Simula um movimento do mouse
-            await Page.Mouse.MoveAsync(400, 300);
-            await Task.Delay(2000);
-
             await Page.GetByPlaceholder("Digite sua senha atual").ClickAsync();
             await Page.Keyboard.TypeAsync("Th0m45@K03n", new() { Delay = 100 });
 
             await Task.Delay(1000);
-
             await Page.GetByLabel("Botão Entrar. Aperte a tecla enter para entrar.").ClickAsync();
+
+            // Execute JavaScript na página para recuperar o elemento pelo atributo 'name'
+            //var txtAreaRespostaCaptcha111 = await Page.EvaluateHandleAsync(@"() => {
+            //    var elements = document.getElementsByName('h-captcha-response');
+            //    return elements.length > 0 ? elements[0] : null;
+            //}");
+
+            var txtAreaRespostaCaptcha = await Page.QuerySelectorAsync("[name='h-captcha-response']");
 
             try
             {
-                QuebrarCaptcha(Page.Url);
+                var result = QuebrarCaptcha(Page.Url);
+
+                if (txtAreaRespostaCaptcha != null)
+                {
+                    await txtAreaRespostaCaptcha.FillAsync(result);
+                }
             }
             catch (Exception ex)
             {
@@ -92,7 +92,7 @@ namespace AntiCaptcha.Api.Controllers
             return true;
         }
 
-        private static void QuebrarCaptcha(string urlAtual)
+        private static string QuebrarCaptcha(string urlAtual)
         {
             DebugHelper.VerboseMode = true;
 
@@ -111,12 +111,18 @@ namespace AntiCaptcha.Api.Controllers
             //api.EnterprisePayload.Add("rqdata", "rqdata value from target website");
             //api.EnterprisePayload.Add("sentry", "true");
 
+            string result = "";
             if (!api.CreateTask())
                 DebugHelper.Out("API v2 send failed. " + api.ErrorMessage, DebugHelper.Type.Error);
             else if (!api.WaitForResult())
                 DebugHelper.Out("Could not solve the captcha.", DebugHelper.Type.Error);
             else
-                DebugHelper.Out("Result: " + api.GetTaskSolution().GRecaptchaResponse, DebugHelper.Type.Success);
+            {
+                result = api.GetTaskSolution().GRecaptchaResponse;
+                DebugHelper.Out("Result: " + result, DebugHelper.Type.Success);
+            }
+
+            return result;
         }
     }
 }
